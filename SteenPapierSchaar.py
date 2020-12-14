c8e949cf37320ee217e329da92c012580c774ab8
sito777 <68819756+sito777@users.noreply.github.com>
2020-12-14 14:51:14 +0100

    s

---
+++ SteenPapierSchaar.py
@@ -0,0 +1,22 @@
+""" De opdracht
+Je gaat steen papier schaar namaken. De regels hiervan zijn bekent maar voor de applicatie moet er een winnaar komen uit een best of 3! (de eerste met 2 punten uit 3 wint). 
+Evenals moet de data worden opgeslagen van degene die heeft gewonnen, met welk voorwerp en hoeveel punten op dat moment.
+
+Eisen V:
+De applicatie blijft aanstaan tot er iemand heeft gewonnen met 2 punten
+De applicatie is werkend en het spel kan gespeeld worden
+De applicatie heeft 2 functies
+De applicatie bevat een 1D & 2D array
+De applicatie kan met 2 spelers gespeeld worden met invoer eigen naam
+De applicatie wordt optijd opgeleverd
+Nette code (variabele)
+
+Eisen G:
+Al het bovenstaande
+Er kan met 3 spelers tegelijk gespeeld worden
+Goede code comments
+"""
+
+introductie = "\nWelkom bij Steen Papier schaar\nOm dit spel te spelen moet je antwoorden met nummers.\n\n1. Speel tegen de PC \n2. Speel een 2 player game\n3. Speel een 3 player game\n"
+print (introductie)
+ 
